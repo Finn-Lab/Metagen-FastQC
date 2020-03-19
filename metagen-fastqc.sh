1bcd43e9b41e6856690ab67801071d823f25f357
Alexandre Almeida <alexandre.m.s.almeida@gmail.com>
2020-03-19 09:12:32 +0000

    Update metagen-fastqc.sh

--- metagen-fastqc.sh
+++ metagen-fastqc.sh
@@ -5,7 +5,7 @@ usage()
 cat << EOF
 usage: $0 options
 
-Clean and remove human DNA sequences from FASTQ files.
+Clean and remove host DNA sequences from FASTQ files.
 
 Needs bsub (-M 15000)
 
@@ -13,6 +13,7 @@ OPTIONS:
    -h      Show help message
    -t      Number of threads (recommended: 16)
    -f      Forward or single-end fastq file (.fastq or *_1.fastq.gz) [REQUIRED]
+   -c      BWA-indexed host genome (.fa)
    -r	   Reverse fastq file (*_2.fastq or *_2.fastq.gz) [OPTIONAL]
 EOF
 }
@@ -20,8 +21,9 @@ EOF
 THREADS=
 FASTQ_R1=
 FASTQ_R2=
+REF=
 
-while getopts “ht:r:f:” OPTION
+while getopts “ht:r:f:c:” OPTION
 do
      case $OPTION in
          h)
@@ -37,6 +39,9 @@ do
 	 r)
              FASTQ_R2=$OPTARG
 	     ;;
+	 c)
+	     REF=$OPTARG
+	     ;;
          ?)
              usage
              exit
@@ -48,16 +53,15 @@ timestamp() {
   date +"%H:%M:%S"
 }
 
-echo "$(timestamp) [remove human sequences] Parsing command-line"
+echo "$(timestamp) [ metagen-fastqc ] Parsing command-line"
 # check if all required arguments are supplied
-if [[ -z ${FASTQ_R1} ]]
+if [[ -z ${FASTQ_R1} ]] || [[ -z ${REF} ]]
 then
      echo "ERROR : Please supply required arguments"
      usage
      exit 1
 fi
 
-REF="/nfs/production/interpro/metagenomics/mags-scripts/bwa_hg38/hg38.fa"
 if [ ${THREADS} -eq 1 ]
 then
     THREADS_SAM=1
@@ -67,28 +71,28 @@ fi
 
 if [[ ! -z ${FASTQ_R2} ]]
 then
-        echo "$(timestamp) [remove human sequences] Cleaning FASTQ files"
+        echo "$(timestamp) [ metagen-fastqc ] Cleaning FASTQ files"
         trim_galore --paired ${FASTQ_R1} ${FASTQ_R2} -o $(dirname ${FASTQ_R1})
         name=${FASTQ_R1%%_1.fastq*}
-        echo "$(timestamp) [remove human sequences] Mapping files to human genome (hg38)"
+        echo "$(timestamp) [ metagen-fastqc ] Mapping files to host genome: ${REF}"
         bwa mem -M -t ${THREADS} ${REF} ${name}_1_val_1.fq.gz ${name}_2_val_2.fq.gz | samtools view -@ ${THREADS_SAM} -f 12 -F 256 -uS - -o ${name}_both_unmapped.bam 
 	samtools sort -@ ${THREADS_SAM} -n ${name}_both_unmapped.bam -o ${name}_both_unmapped_sorted.bam
 	bedtools bamtofastq -i ${name}_both_unmapped_sorted.bam -fq ${name}_clean_1.fastq -fq2 ${name}_clean_2.fastq
-	echo "$(timestamp) [remove human sequences] Compressing output files"
+	echo "$(timestamp) [ metagen-fastqc ] Compressing output files"
 	gzip ${name}_clean_1.fastq
 	gzip ${name}_clean_2.fastq
-        echo "$(timestamp) [remove human sequences] Cleaning tmp files"
+        echo "$(timestamp) [ metagen-fastqc ] Cleaning tmp files"
         rm -rf ${name}_both_unmapped.bam ${name}_both_unmapped_sorted.bam ${name}_*_trimmed.fq.gz ${name}_*_val_*.fq.gz ${name}_*.fastq.gz_*txt
 else
-        echo "$(timestamp) [remove human sequences] Cleaning FASTQ files"
+        echo "$(timestamp) [ metagen-fastqc ] Cleaning FASTQ files"
         trim_galore ${FASTQ_R1} -o $(dirname ${FASTQ_R1})
         name=${FASTQ_R1%%.fastq*}
-        echo "$(timestamp) [remove human sequences] Mapping files to human genome (hg38)"
+        echo "$(timestamp) [ metagen-fastqc ] Mapping files to host genome: ${REF}"
         bwa mem -M -t ${THREADS} ${REF} ${name}_trimmed.fq.gz | samtools view -@ ${THREADS_SAM} -f 4 -F 256 -uS - -o ${name}_unmapped.bam
         samtools sort -@ ${THREADS_SAM} -n ${name}_unmapped.bam -o ${name}_unmapped_sorted.bam
         bedtools bamtofastq -i ${name}_unmapped_sorted.bam -fq ${name}_clean.fastq
-	echo "$(timestamp) [remove human sequences] Compressing output file"
+	echo "$(timestamp) [ metagen-fastqc ] Compressing output file"
 	gzip ${name}_clean.fastq
-        echo "$(timestamp) [remove human sequences] Cleaning tmp files"
+        echo "$(timestamp) [ metagen-fastqc ] Cleaning tmp files"
         rm -rf ${name}_unmapped.bam ${name}_unmapped_sorted.bam ${name}_trimmed.fq.gz ${FASTQ_R1}_*txt
 fi
